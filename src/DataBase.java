5cc08dda2a59c49bec6733d165cc1247c352369d
MaksymSemk <maksumsemen2006@gmail.com>
2024-03-05 12:50:04 +0200

    rewrited tests in DataBase

--- src/DataBase.java
+++ src/DataBase.java
@@ -28,64 +28,65 @@ public class DataBase {
      */
     private DataBase() {
 
-        students = new Student[0];
-        teachers = new Teacher[0];
-        faculties = new Faculty[0];
-        cathedras = new Cathedra[0];
-        groups = new Group[0];
-         /*
-        students = new Student[]{
-                new Student("ЧерненкоАндрійЮрійович", 4,  new Group("ІПЗ", new Cathedra("ІТ" ,new Faculty("ФІ")))),
-                new Student("СеменюкМаксимСеменович", 4,  new Group("ІПЗ", new Cathedra("ІТ" ,new Faculty("ФІ")))),
-                new Student("Гринь Марія n", 2,  new Group("ІПЗ", new Cathedra("ІТ" ,new Faculty("ФІ")))),
-                new Student("Морміль Максим n", 2,  new Group("ІПЗ", new Cathedra("ІТ" ,new Faculty("ФІ")))),
-                new Student("Силенко Ілля Володимирович", 3,  new Group("ІПЗ", new Cathedra("ІТ" ,new Faculty("ФІ")))),
-                new Student("Прохоров Олександр Прохоров", 3,  new Group("ІПЗ", new Cathedra("ІТ" ,new Faculty("ФІ")))),
-                new Student("N", 1,  new Group("ІПЗ", new Cathedra("ІТ" ,new Faculty("ФІ")))),
-                new Student("A", 2,  new Group("ІПЗ", new Cathedra("ІТ" ,new Faculty("ФІ")))),
-                new Student("d", 4,  new Group("ІПЗ", new Cathedra("ІТ" ,new Faculty("ФІ")))),
-                new Student("e", 5,  new Group("ІПЗ", new Cathedra("ІТ" ,new Faculty("ФІ")))),
-        };
-        teachers = new Teacher[]{
-                new Teacher("Глибовець Андрій Миколайович", new Cathedra("ІТ" ,new Faculty("ФІ"))),
-                new Teacher("Глибовець Андрій Миколайович", new Cathedra("ІТ" ,new Faculty("ФІ"))),
-                new Teacher("Митник Юрій null", new Cathedra("ІТ" ,new Faculty("ФІ"))),
-                new Teacher("Гусь Гусь Гусь", new Cathedra("ІТ" ,new Faculty("ФІ"))),
-                new Teacher("Чорней Руслан Костянтинович", new Cathedra("ІТ" ,new Faculty("ФІ"))),
-                new Teacher("Калиновська Оксана В'ячеславівна", new Cathedra("УКР" ,new Faculty("ФІ"))),
-                new Teacher("CALinovska", new Cathedra("УКР" ,new Faculty("ФІ"))),
-                new Teacher("ALinovska", new Cathedra("УКР" ,new Faculty("ФІ"))),
-                new Teacher("Алиновська Оксана В'ячеславівна", new Cathedra("УКР" ,new Faculty("ФІ"))),
-                new Teacher("ЛОЛ", new Cathedra("ІСТОРІЯ" ,new Faculty("ФП"))),
-        };
+//        students = new Student[0];
+//        teachers = new Teacher[0];
+//        faculties = new Faculty[0];
+//        cathedras = new Cathedra[0];
+//        groups = new Group[0];
         faculties = new Faculty[]{new Faculty("ФІ"),
-                    new Faculty("ФСНСТ"),
-                    new Faculty("ФП")};
+                new Faculty("ФСНСТ"),
+                new Faculty("ФП")};
         cathedras = new Cathedra[]{
-                new Cathedra("ІТ" ,new Faculty("ФІ")),
-                new Cathedra("WEBS" ,new Faculty("ФІ")),
-                new Cathedra("УKR" ,new Faculty("ФІ")),
-                new Cathedra("ФС" , new Faculty("ФСНСТ")),
-                new Cathedra("НС" , new Faculty("ФСНСТ")),
-                new Cathedra("СТ" , new Faculty("ФСНСТ")),
-                new Cathedra("ПРАВО" ,new Faculty("ФП")),
-                new Cathedra("ІСТОРІЯ" ,new Faculty("ФП")),
-                new Cathedra("АНГЛМ" ,new Faculty("ФП")),
+                new Cathedra("ІТ" ,faculties[0]),
+                new Cathedra("WEBS" ,faculties[0]),
+                new Cathedra("УKR" ,faculties[0]),
+                new Cathedra("ФС" , faculties[1]),
+                new Cathedra("НС" , faculties[1]),
+                new Cathedra("СТ" , faculties[1]),
+                new Cathedra("ПРАВО" ,faculties[2]),
+                new Cathedra("ІСТОРІЯ" ,faculties[2]),
+                new Cathedra("АНГЛМ" ,faculties[2]),
         };
         groups = new Group[]{
-                new Group("ІПЗ", new Cathedra("ІТ" ,new Faculty("ФІ"))),
-                new Group("КН", new Cathedra("ІТ" ,new Faculty("ФІ"))),
-                new Group("КБ", new Cathedra("ІТ" ,new Faculty("ФІ"))),
-                new Group("ПМ", new Cathedra("ІТ" ,new Faculty("ФІ"))),
-                new Group("ВЕБІНЖ", new Cathedra("WEBS" ,new Faculty("ФІ"))),
-                new Group("РОУТЕРІНЖ", new Cathedra("WEBS" ,new Faculty("ФІ"))),
-                new Group("ФІЛОЛОГІЯ", new Cathedra("УКР" ,new Faculty("ФІ"))),
-                new Group("СОЦІОЛІНГВІСТИКА", new Cathedra("УКР" ,new Faculty("ФІ"))),
-                new Group("ФІЛОСОФІЯ", new Cathedra("ІСТОРІЯ" ,new Faculty("ФП"))),
-                new Group("АНГЛГРАМ", new Cathedra("АНГЛМ" ,new Faculty("ФП"))),
-                new Group("ФІНАНСИ", new Cathedra("СТ" ,new Faculty("ФСНСТ"))),
+                new Group("ІПЗ", cathedras[0]),
+                new Group("КН",cathedras[0]),
+                new Group("КБ",cathedras[0]),
+                new Group("ПМ", cathedras[0]),
+                new Group("ВЕБІНЖ", cathedras[1]),
+                new Group("РОУТЕРІНЖ",cathedras[1]),
+                new Group("ФІЛОЛОГІЯ", cathedras[2]),
+                new Group("СОЦІОЛІНГВІСТИКА", cathedras[2]),
+                new Group("ФІЛОСОФІЯ",  cathedras[7]),
+                new Group("АНГЛГРАМ", cathedras[8]),
+                new Group("ФІНАНСИ",  cathedras[5]),
         };
-*/
+
+
+        students = new Student[]{
+                new Student("ЧерненкоАндрійЮрійович", 4, groups[0]),
+                new Student("СеменюкМаксимСеменович", 4,  groups[0]),
+                new Student("Гринь Марія n", 2,  groups[0]),
+                new Student("Морміль Максим n", 2,  groups[0]),
+                new Student("Силенко Ілля Володимирович", 3, groups[0]),
+                new Student("Прохоров Олександр Прохоров", 3,  groups[0]),
+                new Student("N", 1,  groups[0]),
+                new Student("A", 2,  groups[0]),
+                new Student("d", 4,  groups[0]),
+                new Student("e", 5,  groups[0]),
+        };
+        teachers = new Teacher[]{
+                new Teacher("Глибовець Андрій Миколайович", cathedras[0]),
+                new Teacher("Глибовець Андрій Миколайович", cathedras[0]),
+                new Teacher("Митник Юрій null", cathedras[0]),
+                new Teacher("Гусь Гусь Гусь", cathedras[0]),
+                new Teacher("Чорней Руслан Костянтинович", cathedras[0]),
+                new Teacher("Калиновська Оксана В'ячеславівна", cathedras[2]),
+                new Teacher("CALinovska", cathedras[2]),
+                new Teacher("ALinovska", cathedras[2]),
+                new Teacher("Алиновська Оксана В'ячеславівна", cathedras[2]),
+                new Teacher("ЛОЛ", cathedras[7]),
+        };
+
 
     }
 
